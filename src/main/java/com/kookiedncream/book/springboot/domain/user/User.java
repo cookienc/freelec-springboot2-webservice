b23d5d8a7e4cf769348706e8fc17dacaea996267
kookiedncream <kim76744@naver.com>
2022-02-07 22:43:04 +0900

    fix: oauth 로그인 문제 해결

--- src/main/java/com/kookiedncream/book/springboot/domain/user/User.java
+++ src/main/java/com/kookiedncream/book/springboot/domain/user/User.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Entity
 public class User extends BaseTimeEntity {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false)
