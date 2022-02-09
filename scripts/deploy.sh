ac2d1571121879b8f57ac127c91bf01f86ce7f41
kookiedncream <kim76744@naver.com>
2022-02-09 15:01:18 +0900

    docs: deploy.sh 수정
    applicaion-real -> application-local-real

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
+        -Dspring.config.location=classpath:/application.yml,classpath:/application-local-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
         -Dspring.profiles.active=local-real \
                 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
